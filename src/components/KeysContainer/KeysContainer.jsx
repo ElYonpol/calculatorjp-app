3773590eadd798de9e5dc0e11fbff35e6c74ad32
Juan Pablo Portas Esquivel <jppe@yahoo.com.ar>
2023-05-16 15:49:30 -0300

    v1.0.1

--- src/components/KeysContainer/KeysContainer.jsx
+++ src/components/KeysContainer/KeysContainer.jsx
@@ -67,32 +67,32 @@ export default function KeysContainer() {
 
 			<div className="keyboardContainer">
 				<div className="numbersContainer">
-					<Button id="9" title="9" className="button button-numbers">
-						9
+					<Button id="7" title="7" className="button button-numbers">
+						7
 					</Button>
 					<Button id="8" title="8" className="button button-numbers">
 						8
 					</Button>
-					<Button id="7" title="7" className="button button-numbers">
-						7
-					</Button>
-					<Button id="6" title="6" className="button button-numbers">
-						6
-					</Button>
-					<Button id="5" title="5" className="button button-numbers">
-						5
+					<Button id="9" title="9" className="button button-numbers">
+						9
 					</Button>
 					<Button id="4" title="4" className="button button-numbers">
 						4
 					</Button>
-					<Button id="3" title="3" className="button button-numbers">
-						3
+					<Button id="5" title="5" className="button button-numbers">
+						5
+					</Button>
+					<Button id="6" title="6" className="button button-numbers">
+						6
+					</Button>
+					<Button id="1" title="1" className="button button-numbers">
+						1
 					</Button>
 					<Button id="2" title="2" className="button button-numbers">
 						2
 					</Button>
-					<Button id="1" title="1" className="button button-numbers">
-						1
+					<Button id="3" title="3" className="button button-numbers">
+						3
 					</Button>
 					<Button id="0" title="0" className="button button-numbers">
 						0
